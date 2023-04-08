8a827427c3112da0474f0b9253cd19fa4a1b8e2a
mathieuchartier <mathieu.a.chartier@gmail.com>
2023-04-08 05:14:09 +0000

    sdm632-common: Speed profile services *  Speed profile services and wifi-service to reduce RAM and storage

--- common.mk
+++ common.mk
@@ -397,6 +397,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)/qcom-caf
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
